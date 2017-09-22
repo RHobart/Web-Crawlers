27daeb0b62d8de587c5713458515ec18906c3d13
Hobart <416938380@qq.com>
2017-09-22 09:46:46 +0800

    Add files via upload
    此次修改只是添加异常处理：具体是只要发生异常，就退出浏览器

---
+++ 百度图片动态爬取3.0.py
@@ -0,0 +1,70 @@
+#necoding="utf-8"
+__author__='Hobart'
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
+import urllib.request
+import pymysql
+import re
+import os
+import time 
+#启动谷歌浏览器驱动
+def start_brower(url):
+	driver=webdriver.Chrome()
+	driver.get(url)
+	time.sleep(5)
+	return driver
+#动态获取图片信息
+def get_img_information(location,last_location,count,driver):
+	#当鼠标的位置小于最后的鼠标位置时,循环执行
+	while location<last_location:
+		#获取页面源码
+		html_page=driver.page_source
+		#利用Beautifulsoup4创建soup对象并进行页面解析
+		soup=bs(html_page,"html.parser")
+		time.sleep(2)
+		#通过soup对象中的findAll函数图像信息提取
+		imgs=soup.findAll('img',{'src':re.compile(r'https:.*\.(jpg|png)')})
+		time.sleep(2)
+		#将图片信息存储到Mysql数据库中
+		for img in imgs:
+			print(str(count)+':'+img.get('src')+'\n')
+			name=str(count)
+			style=img.get('src')[-3:]
+			data=img.get('src')
+			cursor.execute("insert into img (img_name,img_stype,img_data) values(%s,%s,%s)",(name,style,data))
+			conn.commit()
+			count+=1
+		time.sleep(2)
+		#通过Selenium模拟鼠标滚动
+		js="$(document).scrollTop(%d)"%location  
+		driver.execute_script(js) 
+		location+=500
+		time.sleep(3)
+		imgs=[]
+
+
+#url="https://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gbk&word=%B7%E7%BE%B0%CD%BC%C6%AC&fr=ala&ala=1&alatpl=adress&pos=0&hs=2&xthttps=111111"
+#url="https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=index&fr=&hs=0&xthttps=111111&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E5%AE%87%E5%AE%99%E5%9B%BE%E7%89%87&oq=yuzhou&rsp=1"
+url="https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=index&fr=&hs=0&xthttps=111111&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E5%85%AB%E5%8D%A6%E5%9B%BE&oq=bagua&rsp=0"
+#启动浏览器，打开网站
+driver=start_brower(url)
+#mysql数据库连接方式
+conn=pymysql.connect(host='localhost',port=3306,user='admin',password='admin',db='img')
+try:	
+	if conn:
+		print("数据库连接成功")
+		cursor=conn.cursor()
+		get_img_information(0,3000,1,driver)
+		cursor.close()
+		conn.close()
+	else:
+		print("数据库连接失败")
+finally:
+	#退出浏览器
+	driver.quit()
+
+
+
+
